0765434492b1afd5442ef6b67683a614ad372329
Yuanzheng Gong <Louis@Yuanzhengs-MacBook-Air.local>
2019-05-27 01:09:11 -0700

    Updates

---
+++ precache-manifest.2f2d5901e4475cc479d319becfb33fd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c02a453825ec07c9e1b8127b1c040a7",
+    "url": "/Connect-4-Web-Game/index.html"
+  },
+  {
+    "revision": "a2f5365d6c508de1b89e",
+    "url": "/Connect-4-Web-Game/static/css/main.623246de.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Connect-4-Web-Game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a2f5365d6c508de1b89e",
+    "url": "/Connect-4-Web-Game/static/js/main.84feb395.chunk.js"
+  },
+  {
+    "revision": "051d699648c5ac2eca98",
+    "url": "/Connect-4-Web-Game/static/js/runtime~main.af981d49.js"
+  }
+]);
